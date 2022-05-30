6c16f51f5baf73446763fe38aa1e49f774c0fac0
Rodrigo <cvc.francisco@gmail.com>
2022-05-30 14:05:34 -0300

    atualizando repositório

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const saudacao = () => {
+    //let h = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', hour12: false });
+    let h = new Date().getHours();
+    if (h <= 5) return 'Boa madrugada';
+    if (h < 12) return 'Bom dia';
+    if (h < 18) return 'Boa tarde';
+    return 'Boa noite';
+  }
+  
+  console.log(saudacao())
+
+  document.getElementById("saudacao").innerHTML = saudacao();
+
+document.getElementById("boasvindas").innerHTML = "Tudo bem? Abaixo você encontrará informações sobre meus projetos e seus respectivos links...";
